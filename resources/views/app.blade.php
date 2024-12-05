bfe6ce79e256b4cd422e03d06c25d02dc28dddd7
waynelogic <prigoda.ad@gmail.com>
2024-12-05 12:18:48 +0300

    PWA FIX

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/pwa/apple-touch-icon.png') }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/pwa/pwa-32x32.png') }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/pwa/pwa-16x16.png') }}">
-{{--        <link rel="manifest" href="{{ asset('/pwa/site.webmanifest') }}">--}}
+        <link rel="manifest" href="{{ asset('/pwa/site.webmanifest') }}">
         <link rel="mask-icon" href="{{ asset('/pws/safari-pinned-tab.svg') }}" color="#5bbad5">
         <link rel="shortcut icon" href="{{ asset('/pwa/favicon.ico') }}">
 {{--        <link rel="manifest" href="{{ asset('/manifest.json') }}">--}}
