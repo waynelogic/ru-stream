c55468a8f92837d64dc6f341847f63a67471bda9
waynelogic <prigoda.ad@gmail.com>
2024-12-06 10:23:30 +0300

    Vibrate

--- resources/js/directives/vibrate.js
+++ resources/js/directives/vibrate.js
@@ -2,7 +2,7 @@ const vVibrate = {
     mounted(el, { value }) {
         el.addEventListener('click', () => {
             if ('vibrate' in navigator) {
-                navigator.vibrate(value || [30]);
+                navigator.vibrate(value || 100);
             }
         });
     }
